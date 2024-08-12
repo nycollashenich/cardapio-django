37fb0ea5872a761aa0243177529b4b6b4428ff8d
Nycollas Henich <nhhenich@gmail.com>
2024-08-12 12:25:28 -0300

    config db

--- app/settings.py
+++ app/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": 'cardapio',
+        "USER": 'postgres',
+        "PASSWORD": 'o76367hhE',
+        "HOST": 'localhost',
+        "PORT": '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
